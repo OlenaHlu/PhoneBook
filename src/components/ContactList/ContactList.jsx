b41a473cf2b1ee8a16847b1463be50282ce6913f
OlenaHlu <148714850+OlenaHlu@users.noreply.github.com>
2024-06-14 11:19:25 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,21 @@
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+import { useSelector } from "react-redux";
+import { selectFilteredContacts } from "../../redux/selectors";
+
+const ContactList = () => {
+  const contacts = useSelector(selectFilteredContacts);
+  return (
+    <ul className={css.component}>
+      {contacts.map((contact) => {
+        return (
+          <li className={css.contactList} key={contact.id}>
+            <Contact contact={contact} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+export default ContactList;
