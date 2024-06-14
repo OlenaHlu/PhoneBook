2252b69f9228be122c54d96d737b8ca3278936e7
OlenaHlu <glushenkova_es@ukr.net>
2024-06-15 00:59:58 +0300

    add HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import DocumentTitle from "../../components/DocumentTitle/DocumentTitle";
+
+function HomePage() {
+  return (
+    <>
+      <DocumentTitle>Home</DocumentTitle>
+      <div>
+        <h1>Welcome to the Phone Book! </h1>
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
